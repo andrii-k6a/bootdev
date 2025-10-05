6317a8bc85066734fc8a8f78c6347656a95f849b
Kook <102363876+andrii-k6a@users.noreply.github.com>
2025-10-05 15:13:27 +0300

    static

--- js-core/class.js
+++ js-core/class.js
@@ -1,3 +1,5 @@
+// console.log(new Band()); // cannot use before decalaration as it is in the temporal dead zone
+
 class Band {
     #compensation;
     constructor(name, members, genre, compensation) {
@@ -46,4 +48,39 @@ console.log(band.tip(10));
 band.tipCounter = 9;
 console.log(band.tip(10));
 // console.log(band.#increaseCompensation(99)); // SynatxError
+
+class Creature {
+    static creaturesCounter = 0; // cannot access it through an instance
+    constructor(description) {
+        this.description = description;
+        Creature.creaturesCounter++;
+    }
+
+    say() {
+        if (Creature.creaturesCounter === 1) {
+            console.log("I'm lonely...");
+        } else if (Creature.creaturesCounter === 2) {
+            console.log("There is someone else!");
+        } else {
+            console.log(`There are ${Creature.creaturesCounter} creatures.`);
+        }
+    }
+
+    static areThereAnyCreatures() {
+        return Creature.creaturesCounter > 0;
+    }
+}
+
+console.log(`Creatures counter ${Creature.creaturesCounter}`);
+console.log(Creature.areThereAnyCreatures());
+
+const pigeon = new Creature("huge pigeon");
+console.log(`Creatures counter ${Creature.creaturesCounter}`);
+console.log(pigeon.description);
+pigeon.say();
+
+const butterfly = new Creature("colorful butterfly");
+console.log(`Creatures counter ${Creature.creaturesCounter}`);
+console.log(butterfly.description);
+butterfly.say();
 
