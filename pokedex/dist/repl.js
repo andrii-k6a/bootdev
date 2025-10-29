4deebfb58c25b9a49b9232647df1f6f6602d02cc
Kook <102363876+andrii-k6a@users.noreply.github.com>
2025-10-29 23:10:28 +0200

    repl init

---
+++ pokedex/dist/repl.js
@@ -0,0 +1,26 @@
+import { createInterface } from 'node:readline';
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+});
+export function cleanInput(input) {
+    return input.toLowerCase()
+        .trim()
+        .split(/\s+/)
+        .filter(i => i.length > 0);
+}
+;
+export function startREPL() {
+    rl.prompt();
+    rl.on("line", line => {
+        const input = cleanInput(line);
+        if (input.length === 0) {
+            rl.prompt();
+            return;
+        }
+        console.log(`Your command was: ${input[0]}`);
+        rl.prompt();
+    });
+}
+;
