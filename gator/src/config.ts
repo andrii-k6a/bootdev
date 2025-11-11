39fda0deeb47419545c971ce8147c797ece59b4c
Kook <102363876+andrii-k6a@users.noreply.github.com>
2025-11-11 15:02:11 +0200

    add configs

---
+++ gator/src/config.ts
@@ -0,0 +1,52 @@
+import fs from "fs";
+import os from "os";
+import path from "path";
+
+export type Config = {
+    dbUrl: string,
+    currentUserName: string,
+}
+
+export function setUser(user: string) {
+    const config = readConfig();
+    config.currentUserName = user;
+    writeConfig(config);
+}
+
+function writeConfig(config: Config): void {
+    const convertedConfig = {
+        db_url: config.dbUrl,
+        current_user_name: config.currentUserName
+    };
+    const configJson = JSON.stringify(convertedConfig, null, 2);
+    const configPath = getConfigFilePath();
+    fs.writeFileSync(configPath, configJson, { encoding: "utf8" });
+}
+
+export function readConfig(): Config {
+    const configPath = getConfigFilePath();
+    const rawConfig = fs.readFileSync(configPath, { encoding: "utf8" });
+    const parsedConfig = JSON.parse(rawConfig);
+    return validateConfig(parsedConfig);
+}
+
+function getConfigFilePath(): string {
+    const configFileName = ".gatorconfig.json";
+    return path.join(os.homedir(), configFileName);
+}
+
+function validateConfig(rawConfig: any): Config {
+    if (!rawConfig.db_url || typeof rawConfig.db_url !== "string") {
+        throw new Error("Missing require property in config: db_url");
+    }
+
+    if (!rawConfig.current_user_name || typeof rawConfig.current_user_name !== "string") {
+        throw new Error("Missing require property in config: current_user_name");
+    }
+
+    return {
+        dbUrl: rawConfig.db_url,
+        currentUserName: rawConfig.current_user_name
+    };
+}
+
