fbb8c0357c8b354b612e2e1f504529d4d450246a
Kook <102363876+andrii-k6a@users.noreply.github.com>
2025-12-27 21:24:25 +0200

    response logging middleware

--- chirpy/src/index.ts
+++ chirpy/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 
 const PORT = 8080;
 const app = express();
 
 app.use("/app", express.static("./src/app"));
+
+app.use(middlewareLogResponse);
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
@@ -15,4 +17,13 @@ app.get("/healthz", handleReadiness);
 async function handleReadiness(request: Request, response: Response) {
     response.set({ "Content-Type": "text/plain; charset=utf-8" });
     response.status(200).send("OK");
+}
+
+function middlewareLogResponse(request: Request, response: Response, next: NextFunction) {
+    response.on("finish", () => {
+        if (response.statusCode !== 200) {
+            console.log(`[NON-OK] ${request.method} ${request.url} - Status: ${response.statusCode}`);
+        }
+    });
+    next();
 }
