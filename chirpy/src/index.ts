e326af1e03e2b9a8fb7306238cf6830beff70d5d
Kook <102363876+andrii-k6a@users.noreply.github.com>
2025-12-28 13:00:36 +0200

    add hit count metric

--- chirpy/src/index.ts
+++ chirpy/src/index.ts
@@ -1,23 +1,6 @@
 import express from "express";
 import type { NextFunction, Request, Response } from "express";
-
-const PORT = 8080;
-const app = express();
-
-app.use("/app", express.static("./src/app"));
-
-app.use(middlewareLogResponse);
-
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
-
-app.get("/healthz", handleReadiness);
-
-async function handleReadiness(request: Request, response: Response) {
-    response.set({ "Content-Type": "text/plain; charset=utf-8" });
-    response.status(200).send("OK");
-}
+import { config } from "./config.js";
 
 function middlewareLogResponse(request: Request, response: Response, next: NextFunction) {
     response.on("finish", () => {
@@ -27,3 +10,41 @@ function middlewareLogResponse(request: Request, response: Response, next: NextF
     });
     next();
 }
+
+function middlewareMetricsInc(request: Request, response: Response, next: NextFunction) {
+    config.fileserverHits++;
+    next();
+}
+
+const app = express();
+
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
+
+app.use(middlewareLogResponse);
+
+async function handleReadiness(request: Request, response: Response) {
+    response.set({ "Content-Type": "text/plain; charset=utf-8" });
+    response.status(200).send("OK");
+}
+
+app.get("/healthz", handleReadiness);
+
+async function handleFileserverHits(request: Request, response: Response) {
+    response.set({ "Content-Type": "text/plain; charset=utf-8" });
+    response.status(200).send(`Hits: ${config.fileserverHits}`);
+}
+
+app.get("/metrics", handleFileserverHits);
+
+async function handleResetMetrics(request: Request, response: Response) {
+    config.fileserverHits = 0;
+    response.status(200).send("Hits reset to 0");
+}
+
+app.get("/reset", handleResetMetrics);
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+
